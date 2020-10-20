945cdd23556a61a864d9c1cd551f05287aa5414c
Travis Bailey <travis@inwego.com>
2020-10-20 09:32:50 -0400

    adding tests to validate gift functionality

--- spec/features/purchase_product_spec.rb
+++ spec/features/purchase_product_spec.rb
@@ -69,4 +69,44 @@ RSpec.feature "Purchase Product", type: :feature do
     expect(page).to have_content(Order.last.user_facing_id)
     expect(page).to have_content("Kim Jones")
   end
+
+  scenario "Allows for a Gift Purchase" do
+    Product.create!(
+        name: "product1",
+        description: "description2",
+        price_cents: 1000,
+        age_low_weeks: 0,
+        age_high_weeks: 12,
+        )
+    Child.create!(
+        full_name: "Kim Jones",
+        birthdate: Date.parse("2019-03-03"),
+        parent_name: "Pat Jones"
+    )
+
+    visit "/"
+
+    within ".products-list .product" do
+      click_on "More Details…"
+    end
+
+    click_on "Buy Now $10.00"
+
+    check "order[is_gift]"
+    fill_in "order[gift_message]", with: "From Nanna"
+    fill_in "order[credit_card_number]", with: "4111111111111111"
+    fill_in "order[expiration_month]", with: 12
+    fill_in "order[expiration_year]", with: 25
+    fill_in "order[shipping_name]", with: "Pat Jones"
+    fill_in "order[child_full_name]", with: "Kim Jones"
+    fill_in "order[child_birthdate]", with: "2019-03-03"
+
+    click_on "Purchase"
+
+    expect(page).to have_content("Thanks for Your Order")
+    expect(page).to have_content(Order.last.user_facing_id)
+    expect(page).to have_content("Kim Jones")
+
+
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   describe "#validations" do
+    it "minimal valid requirements" do
+      order = Order.new(
+        product: Product.new,
+        child: Child.new,
+        shipping_name: "Some name"
+      )
+      expect(order.valid?).to eq(true)
+      expect(order.is_gift).to eq(false)
+    end
+
     it "requires shipping_name" do
       order = Order.new(
         product: Product.new,
